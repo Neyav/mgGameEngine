376fc0850402448ea0342f30dc23c4e3ea67f6aa
Neyav <dashevil@gmail.com>
2015-04-04 12:00:32 -0600

    mgMapElement if changed to switch
    switch/case is faster than if/else. Until I decide whether or not
    mgMapElement is going to be a base class for polymorphism ( probably
    unnecessary ) this will better facilitate lots of block definition
    types.

--- Source/mgMapElement.cpp
+++ Source/mgMapElement.cpp
@@ -7,60 +7,63 @@ void mgMapElement::GenerateBlockGeometry(void)
 	if (BlockShape != NULL)
 		delete BlockShape;
 
-	if (BlockType == MAP_BLOCKFLOOR)
+	switch (BlockType)
 	{
-		BlockShape = NULL;
-	}
-	else if (BlockType == MAP_BLOCKWALL)
-	{
-		mgLineSegment *ShapeLine;
-		mgPoint Start, End;
+	case MAP_BLOCKFLOOR: // Floors don't have any shape.
+			BlockShape = NULL;
+			break;
+	case MAP_BLOCKWALL: // Walls have a very simple orthagonical block shape.
+			mgLineSegment *ShapeLine;
+			mgPoint Start, End;
 
-		BlockShape = new mgLinkedList<mgLineSegment>;
+			BlockShape = new mgLinkedList < mgLineSegment > ;
 
-		// Defined shape of an orthogonical blocking wall
+			// Defined shape of an orthogonical blocking wall
+			// true in the AddElementReference call defines this object, this list, as being the one that owns the
+			// mgLineSegment pointer. This ownership means that it is the one responsible for deleting it upon cleanup.
 
-		// 0,0 - 0,1
-		ShapeLine = new mgLineSegment;
-		Start.Y = 0 + Position.Y;
-		Start.X = 0 + Position.X;
-		End.Y = 0 + Position.Y;
-		End.X = 1 + Position.X;
-		ShapeLine->LineSegmentBlock = this;
-		ShapeLine->LineSegmentOwner = NULL;
-		ShapeLine->Facing = LINEFACE_RIGHT;
-		ShapeLine->ImportLine(Start, End);
-		BlockShape->AddElementReference(ShapeLine, true);
+			// 0,0 - 0,1
+			ShapeLine = new mgLineSegment;
+			Start.Y = 0 + Position.Y;
+			Start.X = 0 + Position.X;
+			End.Y = 0 + Position.Y;
+			End.X = 1 + Position.X;
+			ShapeLine->LineSegmentBlock = this;
+			ShapeLine->LineSegmentOwner = NULL;
+			ShapeLine->Facing = LINEFACE_RIGHT;
+			ShapeLine->ImportLine(Start, End);
+			BlockShape->AddElementReference(ShapeLine, true);
 
-		// 0,1 - 1,1
-		ShapeLine = new mgLineSegment;
-		Start.X++;
-		End.Y++;
-		ShapeLine->LineSegmentBlock = this;
-		ShapeLine->LineSegmentOwner = NULL;
-		ShapeLine->Facing = LINEFACE_RIGHT;
-		ShapeLine->ImportLine(Start, End);
-		BlockShape->AddElementReference(ShapeLine, true);
+			// 0,1 - 1,1
+			ShapeLine = new mgLineSegment;
+			Start.X++;
+			End.Y++;
+			ShapeLine->LineSegmentBlock = this;
+			ShapeLine->LineSegmentOwner = NULL;
+			ShapeLine->Facing = LINEFACE_RIGHT;
+			ShapeLine->ImportLine(Start, End);
+			BlockShape->AddElementReference(ShapeLine, true);
 
-		// 1,1 - 1,0
-		ShapeLine = new mgLineSegment;
-		Start.Y++;
-		End.X--;
-		ShapeLine->LineSegmentBlock = this;
-		ShapeLine->LineSegmentOwner = NULL;
-		ShapeLine->Facing = LINEFACE_RIGHT;
-		ShapeLine->ImportLine(Start, End);
-		BlockShape->AddElementReference(ShapeLine, true);
+			// 1,1 - 1,0
+			ShapeLine = new mgLineSegment;
+			Start.Y++;
+			End.X--;
+			ShapeLine->LineSegmentBlock = this;
+			ShapeLine->LineSegmentOwner = NULL;
+			ShapeLine->Facing = LINEFACE_RIGHT;
+			ShapeLine->ImportLine(Start, End);
+			BlockShape->AddElementReference(ShapeLine, true);
 
-		// 1,0 - 0,0
-		ShapeLine = new mgLineSegment;
-		Start.X--;
-		End.Y--;
-		ShapeLine->LineSegmentBlock = this;
-		ShapeLine->LineSegmentOwner = NULL;
-		ShapeLine->Facing = LINEFACE_RIGHT;
-		ShapeLine->ImportLine(Start, End);
-		BlockShape->AddElementReference(ShapeLine, true);
+			// 1,0 - 0,0
+			ShapeLine = new mgLineSegment;
+			Start.X--;
+			End.Y--;
+			ShapeLine->LineSegmentBlock = this;
+			ShapeLine->LineSegmentOwner = NULL;
+			ShapeLine->Facing = LINEFACE_RIGHT;
+			ShapeLine->ImportLine(Start, End);
+			BlockShape->AddElementReference(ShapeLine, true);
+			break;
 	}
 }
 
