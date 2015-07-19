5956f44ca86ee1eab2ec493c1e282db69871e3ae
Chris Laverdure <dashevil@gmail.com>
2015-07-19 11:12:22 -0600

    Tie SDLengine together with mgGameEngine components

--- Source/SDLengine/SDLengine.cpp
+++ Source/SDLengine/SDLengine.cpp
@@ -3,39 +3,43 @@
 #include <stdio.h>
 #include <string>
 
+// SDLengine includes
 #include "SERenderHandler.h"
 #include "SETextureHandler.h"
+
+// mgGameEngine includes
+#include "../mgMapDataHandler.h"
+#include "../mgMapElement.h"
+#include "../mgMapObject.h"
+#include "../mgVectorPoint.h"
+#include "../mgLineSegment.h"
 
 int SCREENWIDTH = 1280;
 int SCREENHEIGHT = 720;
 
-int tileswide = 8;
-
-int tilewidth = SCREENWIDTH / tileswide;
-int tileheight = SCREENHEIGHT / tileswide;
+SERenderHandler *RenderEngine = nullptr;
+mgMapDataHandler *GameWorld = nullptr;
 
 int main ( void )
 {
-	SERenderHandler RenderEngine;
 	int i = 0;
+	
+	RenderEngine = new SERenderHandler;
+	RenderEngine->InitWindow(SCREENWIDTH,SCREENHEIGHT, "SDL Engine test");
 
-	RenderEngine.InitWindow(SCREENWIDTH,SCREENHEIGHT, "SDL Engine test");
-
-	SETextureHandler FloorTexture(&RenderEngine); // Cannot be called until after the Renderer has been setup.
-	SETextureHandler WallTexture(&RenderEngine);
+	SETextureHandler FloorTexture(RenderEngine); // Cannot be called until after the Renderer has been setup.
+	SETextureHandler WallTexture(RenderEngine);
 	FloorTexture.loadFromFile("SDLenginefloor.png");
 	WallTexture.loadFromFile("SDLenginewall.png");
 
-	FloorTexture.setSize(tilewidth, tileheight);
-
 	while(1)
 	{
-		RenderEngine.ClearScreen();
+		RenderEngine->ClearScreen();
 		i++;
 		if (i > 700)
 			i = 0;	
 		FloorTexture.render(i,i);
 		WallTexture.render(700-i,700-i);
-		RenderEngine.UpdateScreen();
+		RenderEngine->UpdateScreen();
 	}
 }
