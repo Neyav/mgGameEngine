4089a9a31c70db2acc37753e549a8af2840550f6
Chris Laverdure <dashevil@gmail.com>
2015-07-28 13:56:49 -0600

    Well, okay, these were wrong. As proven on tests with lines that were not going straight up/down or left/right.

--- Source/mgCollisionDetection.cpp
+++ Source/mgCollisionDetection.cpp
@@ -79,7 +79,7 @@ void mgCollisionDetection::SetupDetectionArea(unsigned int Range) // Stage Two
 					// and we can't exclude it on that basis alone.
 					if ( AttemptedMovement.Y > 0 ) 
 					{
-						if ( mgSmallestValue(LineSegRef->SegmentStart.Y, LineSegRef->SegmentEnd.Y ) > MovingObject->Position.Y + MovingObject->ObjectSize ) 
+						if ( mgSmallestValue(LineSegRef->SegmentStart.Y, LineSegRef->SegmentEnd.Y ) < MovingObject->Position.Y + MovingObject->ObjectSize ) 
 							exitLoop = false;
 
 						if ( MovingObject->Position.Y + MovingObject->ObjectSize + AttemptedMovement.Y <
@@ -88,7 +88,7 @@ void mgCollisionDetection::SetupDetectionArea(unsigned int Range) // Stage Two
 					}
 					else if ( AttemptedMovement.Y < 0 )
 					{
-						if ( mgLargestValue(LineSegRef->SegmentStart.Y, LineSegRef->SegmentEnd.Y ) < MovingObject->Position.Y - MovingObject->ObjectSize )
+						if ( mgLargestValue(LineSegRef->SegmentStart.Y, LineSegRef->SegmentEnd.Y ) > MovingObject->Position.Y - MovingObject->ObjectSize )
 							exitLoop = false;
 
 						if ( MovingObject->Position.Y - MovingObject->ObjectSize + AttemptedMovement.Y >
@@ -99,7 +99,7 @@ void mgCollisionDetection::SetupDetectionArea(unsigned int Range) // Stage Two
 
 					if ( AttemptedMovement.X > 0 )
 					{ 
-						if ( mgSmallestValue(LineSegRef->SegmentStart.X, LineSegRef->SegmentEnd.X ) > MovingObject->Position.X + MovingObject->ObjectSize )
+						if ( mgSmallestValue(LineSegRef->SegmentStart.X, LineSegRef->SegmentEnd.X ) < MovingObject->Position.X + MovingObject->ObjectSize )
 							exitLoop = false;
 
 						if ( MovingObject->Position.X + MovingObject->ObjectSize + AttemptedMovement.X <
@@ -108,7 +108,7 @@ void mgCollisionDetection::SetupDetectionArea(unsigned int Range) // Stage Two
 					}
 					else if ( AttemptedMovement.X < 0 )
 					{
-						if ( mgLargestValue(LineSegRef->SegmentStart.X, LineSegRef->SegmentEnd.X ) < MovingObject->Position.X - MovingObject->ObjectSize )
+						if ( mgLargestValue(LineSegRef->SegmentStart.X, LineSegRef->SegmentEnd.X ) > MovingObject->Position.X - MovingObject->ObjectSize )
 							exitLoop = false;
 
 						if ( MovingObject->Position.X - MovingObject->ObjectSize + AttemptedMovement.X >
